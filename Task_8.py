94a5cb3c212c8a2dec9d40a5829a79d7910ce8b0
Kostik <klik@example.com>
2023-08-13 17:33:59 +0300

    Задача №8

---
+++ Task_8.py
@@ -0,0 +1,20 @@
+# Создать страницу, на которой будет форма для ввода имени
+# и кнопка "Отправить"
+# При нажатии на кнопку будет произведено
+# перенаправление на страницу с flash сообщением, где будет
+# выведено "Привет, {имя}!".
+
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+@app.route('/name/', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        return f'Hello {name}!'
+    return render_template('name.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
